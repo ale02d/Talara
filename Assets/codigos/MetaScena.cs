d634ddf65bb87134b0e6d41fa9e167381d563de5
ale02d <139078644+ale02d@users.noreply.github.com>
2023-07-09 16:44:42 -0500

    Initial commit

---
+++ Assets/codigos/MetaScena.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MetaScena : MonoBehaviour
+{
+     public  int nextScene;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            SceneManager.LoadScene(nextScene);
+            Debug.Log("hit Scene");
+        }
+        Debug.Log("hit Scene");
+    }
+    
+}
